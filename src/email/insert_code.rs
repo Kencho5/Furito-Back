17713c5e617eed884c34e328ba3fdd28da776251
Kencho5 <giokenchadze@gmail.com>
2025-02-13 18:24:46 +0400

    Removed: debug comments

--- src/email/insert_code.rs
+++ src/email/insert_code.rs
@@ -3,7 +3,7 @@ use sqlx::Error;
 
 pub async fn insert_code(pool: PgPool, email: String, code: u16) -> Result<(), Error> {
     sqlx::query(
-        "INSERT INTO email_codes(email, code, expiry) VALUES($1, $2, NOW() + INTERVAL '0 minutes')",
+        "INSERT INTO email_codes(email, code, expiry) VALUES($1, $2, NOW() + INTERVAL '5 minutes')",
     )
     .bind(&email)
     .bind(code as i32)
--- src/routes/verify/verify_email.rs
+++ src/routes/verify/verify_email.rs
@@ -7,14 +7,14 @@ pub async fn verify_email_handler(
     Json(payload): Json<VerifyEmailPayload>,
 ) -> Result<StatusCode, VerifyEmailError> {
     let code: u16 = rand::random_range(1000..9999);
-    //send_email(
-    //    state.ses_client,
-    //    &payload.email,
-    //    "Verify email".to_string(),
-    //    Some(code.to_string()),
-    //)
-    //.await
-    //.map_err(|_| VerifyEmailError::InvalidEmail)?;
+    send_email(
+        state.ses_client,
+        &payload.email,
+        "Verify email".to_string(),
+        Some(code.to_string()),
+    )
+    .await
+    .map_err(|_| VerifyEmailError::InvalidEmail)?;
 
     insert_code(state.pool, payload.email, code)
         .await
