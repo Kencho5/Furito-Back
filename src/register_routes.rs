53dca8f80f1a9231428516e89bb04e36ae8660bc
Kencho5 <giokenchadze@gmail.com>
2025-02-15 11:45:38 +0400

    Added: org route

--- src/register_routes.rs
+++ src/register_routes.rs
@@ -2,13 +2,20 @@ use crate::prelude::*;
 use crate::routes::*;
 
 pub fn create_router() -> Router<AppState> {
-    Router::new().merge(auth_routes())
+    Router::new()
+        .merge(auth_routes())
+        .merge(verify_routes())
+        .merge(org_routes())
 }
 
 fn auth_routes() -> Router<AppState> {
     Router::new()
         .route("/login", post(auth::login::login_handler))
         .route("/register", post(auth::register::register_handler))
+}
+
+fn verify_routes() -> Router<AppState> {
+    Router::new()
         .route(
             "/verify-email",
             post(verify::verify_email::verify_email_handler),
@@ -17,4 +24,8 @@ fn auth_routes() -> Router<AppState> {
             "/verify-email-code",
             post(verify::verify_email::verify_email_code_handler),
         )
+}
+
+fn org_routes() -> Router<AppState> {
+    Router::new().route("/add-org", post(organizations::add_org::add_org_handler))
 }
