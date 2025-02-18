b01665ad92fa4e3dbf7252680756a8508dcd426c
Kencho5 <giokenchadze@gmail.com>
2025-02-18 12:08:25 +0400

    update: rate limit seconds to ms

--- src/rate_limit.rs
+++ src/rate_limit.rs
@@ -2,7 +2,7 @@
 macro_rules! rate_limit {
     ($duration:expr) => {
         RateLimitLayer::<RealIp>::builder()
-            .with_default_quota(Quota::simple(std::time::Duration::from_secs($duration)))
+            .with_default_quota(Quota::simple(std::time::Duration::from_millis($duration)))
             .default_handle_error()
     };
 }
--- src/register_routes.rs
+++ src/register_routes.rs
@@ -17,9 +17,9 @@ pub fn create_router() -> Router<AppState> {
 fn auth_routes() -> Router<AppState> {
     Router::new()
         .route("/login", post(auth::login::login_handler))
-        .route_layer(rate_limit!(2))
+        .route_layer(rate_limit!(2000))
         .route("/register", post(auth::register::register_handler))
-        .route_layer(rate_limit!(3))
+        .route_layer(rate_limit!(3000))
 }
 
 fn verify_routes() -> Router<AppState> {
@@ -28,18 +28,19 @@ fn verify_routes() -> Router<AppState> {
             "/verify-email",
             post(verify::verify_email::verify_email_handler),
         )
-        .route_layer(rate_limit!(3))
+        .route_layer(rate_limit!(3000))
         .route(
             "/verify-email-code",
             post(verify::verify_email::verify_email_code_handler),
         )
-        .route_layer(rate_limit!(1))
+        .route_layer(rate_limit!(1000))
 }
 
 fn org_routes() -> Router<AppState> {
     Router::new()
         .route("/add-org", post(organizations::add_org::add_org_handler))
+        .route_layer(rate_limit!(2000))
         .route("/get-orgs", post(organizations::get_orgs::get_orgs_handler))
-        .route_layer(rate_limit!(5))
+        .route_layer(rate_limit!(500))
         .route_layer(middleware::from_fn(validate_headers))
 }
